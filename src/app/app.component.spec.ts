d940d6147944863a0f10826686590042598fdc46
Felix Itzenplitz <felix@stkn.org>
2017-11-13 16:41:17 +0100

    add test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,4 +17,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('angular-highcharts demo');
+  }));
 });
