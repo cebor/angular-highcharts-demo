0c042b2a1eda53bde5c698dabd6aae1f8ad50cab
Felix Itzenplitz <felix@stkn.org>
2018-05-03 23:51:56 +0200

    update to angular6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
-import more from 'highcharts/highcharts-more.src';
+import * as more from 'highcharts/highcharts-more.src';
+import * as exporting from 'highcharts/modules/exporting.src';
 
 import { AppComponent } from './app.component';
 
-export function highchartsModules() {
-  return [more];
-}
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ChartModule
-  ],
-  providers: [
-    { provide: HIGHCHARTS_MODULES, useFactory: highchartsModules }
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChartModule],
+  providers: [{ provide: HIGHCHARTS_MODULES, useFactory: () => [more, exporting] }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
